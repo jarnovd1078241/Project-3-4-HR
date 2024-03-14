703a0d6e7f2b74b0f578066a8200084e93659829
Martijn-v-Dijk <153369752+Martijn-v-Dijk@users.noreply.github.com>
2024-03-14 11:55:05 +0100

    Keypad
    Code voor de keypad

---
+++ Keypad.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'C'}, // C staat voor Cancel
+  {'4', '5', '6', 'L'}, // L staat voor Clear
+  {'7', '8', '9', 'E'}, // E staat voor Enter
+  {' ', '0', ' ', ' '}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6}; 
+byte colPins[COLS] = {5, 4, 3, 2}; 
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
+}
